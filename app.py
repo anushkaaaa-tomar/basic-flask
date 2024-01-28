aa628b5fb0ed8005b9c1a612805626dab060415a
Anushka Tomar <146758188+anushkaaaa-tomar@users.noreply.github.com>
2024-01-28 22:14:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
-
 app = Flask(__name__)
-
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "{\"message\":\"hey there python\"}"
+if __name__ == "__main__":
+    app.hello_world(host="0.0.0.0", port=int("3000"), debug=True)
